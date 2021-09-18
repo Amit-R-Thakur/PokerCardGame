38133b21236ec6f74d31690ca1ca02fde84b934d
Amit_R_Thakur <at913513@gmail.com>
2021-09-18 21:07:36 +0530

    Bet Coin Received functionality Added to the Server

--- public/script/clientServer.js
+++ public/script/clientServer.js
@@ -1,272 +1,315 @@
+let socket = io();
+
+let rupeesIcon=`<i class="fas fa-rupee-sign"></i>`;
 //Array For Profile Image 
-let PrfImageList=["../images/face1.jpg","../images/face2.jpg","../images/face3.jpg","../images/face4.jpg","../images/face5.jpg","../images/face6.jpg","../images/face7.jpg","../images/face8.jpg","../images/face9.jpg","../images/face10.jpg","../images/face11.jpg","../images/face12.jpg","../images/face13.jpg"];
-let table=["table1","table2","table3","table4","table5"];
-let addProfileImage="../images/addUser.jpg";
-let backSideOfCardIs="../CardImage/blue_back.png";
+let PrfImageList = ["../images/face1.jpg", "../images/face2.jpg", "../images/face3.jpg", "../images/face4.jpg", "../images/face5.jpg", "../images/face6.jpg", "../images/face7.jpg", "../images/face8.jpg", "../images/face9.jpg", "../images/face10.jpg", "../images/face11.jpg", "../images/face12.jpg", "../images/face13.jpg"];
+let table = ["table1", "table2", "table3", "table4", "table5"];
+let addProfileImage = "../images/addUser.jpg";
+let backSideOfCardIs = "../CardImage/blue_back.png";
 let MyChangesTable;
 let MyChangesCard1;
 let MyChangesCard2;
 
 //Function For reaturn random Table..........
-function RandomTable()
-{
-    let random=Math.floor(Math.random()*table.length);
-    let Randomtable=table[random];
-    table.splice(random,1);
+function RandomTable() {
+    let random = Math.floor(Math.random() * table.length);
+    let Randomtable = table[random];
+    table.splice(random, 1);
     return Randomtable;
 }
 //Function For return Random PrfImage
-function RandomProfileImage()
-{
-    let random=Math.floor(Math.random()*PrfImageList.length);
-    let image=PrfImageList[random];
-    PrfImageList.splice(random,1);
+function RandomProfileImage() {
+    let random = Math.floor(Math.random() * PrfImageList.length);
+    let image = PrfImageList[random];
+    PrfImageList.splice(random, 1);
     return image;
 }
 
 //Function for Manage Coin (get Coin or Set Coin To Loacal Storage)
-document.addEventListener("DOMContentLoaded",()=>{
-function setCoinToLocalStorage()
-{
-    let coin =localStorage.getItem("MyPokerGameCoin");
-    if(coin==null)
-    {
-        localStorage.setItem("MyPokerGameCoin",100000);
-        coin =localStorage.getItem("MyPokerGameCoin");
+document.addEventListener("DOMContentLoaded", () => {
+    function setCoinToLocalStorage() {
+        let coin = localStorage.getItem("MyPokerGameCoin");
+        if (coin == null) {
+            localStorage.setItem("MyPokerGameCoin", 100000);
+            coin = localStorage.getItem("MyPokerGameCoin");
+            return coin;
+        }
         return coin;
     }
-    return coin;
-}
 
 
-//Function for set Coin to My Coin
-setCoinToMyCoin();
-function setCoinToMyCoin()
-{
-    document.querySelector("#MyCoin").innerText=setCoinToLocalStorage();
-}
+    //Function for set Coin to My Coin
+    setCoinToMyCoin();
+    function setCoinToMyCoin() {
+        document.querySelector("#MyCoin").innerText = setCoinToLocalStorage();
+    }
+
+
+    
+   //Logic For Bet of code Game....................
+   SendBetCoinToServer();
+   function SendBetCoinToServer()
+   {
+       
+       document.querySelector("#call").addEventListener("click",()=>{
+        
+           let BetCoin=Number(document.querySelector("#rs").innerText);
+           let userId=socket.id;
+           let dataToSend={"userId":userId,"BetCoin":BetCoin};
+           socket.emit("BetCoin",dataToSend);
+
+
+       });
+
+   }
+   
+
+
 });
 //Function For Real Time Message...........
-function Message(msg)
-{
-    let mainBox=document.querySelector(".msgBox");
-    mainBox.innerHTML=`<h2>${msg}</h2>`;
+function Message(msg) {
+    let mainBox = document.querySelector(".msgBox");
+    mainBox.innerHTML = `<h2>${msg}</h2>`;
 }
 //Function For set Time For Starting game......
-function setTime(tm)
-{
-    let  spn=document.querySelector("#time");
+function setTime(tm) {
+    let spn = document.querySelector("#time");
     console.log(spn);
-    spn.innerText=tm;
+    spn.innerText = tm;
 
 }
 //Function  for set Game Start Message.............
-function sendGameStartMessage(msg)
-{
-    document.querySelector("#h2").innerText=msg;
+function sendGameStartMessage(msg) {
+    document.querySelector("#h2").innerText = msg;
 }
 //Function For return table class....
-function returnTableClass(table)
-{
+function returnTableClass(table) {
     let Newtable;
-    if(table=="table1")Newtable="t1";
-    else if(table=="table2")Newtable="t2";
-    else if(table=="table3")Newtable="t3";
-    else if(table=="table4")Newtable="t4";
-    else if(table=="table5")Newtable="t5";
+    if (table == "table1") Newtable = "t1";
+    else if (table == "table2") Newtable = "t2";
+    else if (table == "table3") Newtable = "t3";
+    else if (table == "table4") Newtable = "t4";
+    else if (table == "table5") Newtable = "t5";
     return Newtable;
 }
 //Function For return Card Id.........
-function returnCardId(table)
-{
+function returnCardId(table) {
     let num;
-    if(table=="t1")num=1;
-    else if(table=="t2")num=2;
-    else if(table=="t3")num=3;
-    else if(table=="t4")num=4;
-    else if(table=="t5")num=5;
-    let card={};
-    card["card1"]=`p${num}card1`;
-    card["card2"]=`p${num}card2`;
-    if(num==5)
-    {
-        card["card1"]=`Mycard1`;
-    card["card2"]=`Mycard2`;
-        
+    if (table == "t1") num = 1;
+    else if (table == "t2") num = 2;
+    else if (table == "t3") num = 3;
+    else if (table == "t4") num = 4;
+    else if (table == "t5") num = 5;
+    let card = {};
+    card["card1"] = `p${num}card1`;
+    card["card2"] = `p${num}card2`;
+    if (num == 5) {
+        card["card1"] = `Mycard1`;
+        card["card2"] = `Mycard2`;
+
     }
-   return card;
+    return card;
 
 }
 //Function for Return Card Image Url.........
-function ReturnCardImageUrl(card)
-{
+function ReturnCardImageUrl(card) {
     return `../CardImage/${card}.png`;
+}
+//Function For Receved Coin..................
+function RecivedCoin(data)
+{
+     if(data==socket.id)
+    {
+        document.querySelector(".btnMainDiv").classList.remove("none");
+       
+       document.querySelector("#call").addEventListener("click",()=>{
+           console.log(BetCoin);
+        
+        socket.emit("RecevedCoin",socket.id,BetCoin);
+       })
+    }
+
 }
 
 
-let socket =io();
-let Name=prompt("Enter Your Name to Join");
-socket.emit("newUser",Name);
+
+let Name = prompt("Enter Your Name to Join");
+socket.emit("newUser", Name);
+
+let BetCoin;
+socket.on("PlayGame1",(data)=>{
+    RecivedCoin(data);
+    BetCoin=document.querySelector("#rs").innerText;
+});
+socket.on("PlayGame2",(data)=>{
+    RecivedCoin(data);
+    BetCoin=document.querySelector("#rs").innerText;
+});
+socket.emit("RecevedCoin",socket.id,BetCoin);
 
 
+socket.on("userJoin", (data) => {
 
-socket.on("userJoin",(data)=>
-{
-    socket.on("PlayGame",(data)=>{
-        data.forEach((elm)=>{
-        if(elm.userId==socket.id)
-        {
-            document.querySelector(".btnMainDiv").classList.remove("none");
-            let BetCoin=document.querySelector("#rs").innerText;
-           document.querySelector("#call").addEventListener("click",()=>{
-            socket.emit("RecevedCoin",socket.id,BetCoin);
-           })
-        }
-
-        })
-    })
-    socket.on("gameStartedOrNot",(data)=>{
+   
+    socket.on("gameStartedOrNot", (data) => {
         console.log(data);
         sendGameStartMessage(data);
 
     })
-    socket.on("sendTime",(data)=>{
+    socket.on("sendTime", (data) => {
         setTime(data);
     })
-    socket.on("sendGameStartMessage",(data)=>{
-        let msg=`game Started`;
+    socket.on("sendGameStartMessage", (data) => {
+        let msg = `game Started`;
         sendGameStartMessage(msg);
     })
-    let msg=`welcome ${data}`;
+    let msg = `welcome ${data}`;
     Message(msg);
-    socket.on("cardDistribution",(data)=>{
-      
-        data.forEach((elm)=>{
-         if(elm.userId==socket.id)
-         {
-            let table=returnTableClass(elm.table);
-            let cardLocation=returnCardId("t5");
-            let card1=ReturnCardImageUrl(elm.card1);
-            let card2=ReturnCardImageUrl(elm.card2);
-            console.log(cardLocation);
-            console.log(cardLocation.card1);
-            document.querySelector(`#Mycard1`).style.backgroundImage=`url("${card1}")`;
-            document.querySelector(`#Mycard2`).style.backgroundImage=`url("${card2}")`;
+    socket.on("cardDistribution", (data) => {
 
-         }
-         else
-         {
-             if(elm.table=="table5")
-             {
-                document.querySelector(`#${MyChangesCard1}`).style.backgroundImage=`url("${backSideOfCardIs}")`;
-                document.querySelector(`#${MyChangesCard2}`).style.backgroundImage=`url("${backSideOfCardIs}")`;
-               
+        data.forEach((elm) => {
+            if (elm.userId == socket.id) {
+                let table = returnTableClass(elm.table);
+                let cardLocation = returnCardId("t5");
+                let card1 = ReturnCardImageUrl(elm.card1);
+                let card2 = ReturnCardImageUrl(elm.card2);
+                console.log(cardLocation);
+                console.log(cardLocation.card1);
+                document.querySelector(`#Mycard1`).style.backgroundImage = `url("${card1}")`;
+                document.querySelector(`#Mycard2`).style.backgroundImage = `url("${card2}")`;
 
-             }
-             else
-             {
-            let table=returnTableClass(elm.table);
-            let cardLocation=returnCardId(table);
-            document.querySelector(`#${cardLocation.card1}`).style.backgroundImage=`url("${backSideOfCardIs}")`;
-            document.querySelector(`#${cardLocation.card2}`).style.backgroundImage=`url("${backSideOfCardIs}")`;
-           }
+            }
+            else {
+                if (elm.table == "table5") {
+                    document.querySelector(`#${MyChangesCard1}`).style.backgroundImage = `url("${backSideOfCardIs}")`;
+                    document.querySelector(`#${MyChangesCard2}`).style.backgroundImage = `url("${backSideOfCardIs}")`;
 
-         }
+
+                }
+                else {
+                    let table = returnTableClass(elm.table);
+                    let cardLocation = returnCardId(table);
+                    document.querySelector(`#${cardLocation.card1}`).style.backgroundImage = `url("${backSideOfCardIs}")`;
+                    document.querySelector(`#${cardLocation.card2}`).style.backgroundImage = `url("${backSideOfCardIs}")`;
+                }
+
+            }
 
         })
-        
-    
-    });
-   
-    socket.on("sendMessge",(data)=>{
-        let msg=`${data} join the table.`;
-    Message(msg);
-    });
-   socket.on("sendBookedImageAndTable",(data,data2,data3)=>{
-    console.log(data);
-    if(data=="table5")
-    {
-        let ptbl=document.querySelector(`.${MyChangesTable}`);
-        ptbl.style.backgroundImage=`url("${data2}")`;
-          
 
-    }
-    else
-    {
-        let ptbl=document.querySelector(`.${data}`);
-        ptbl.style.backgroundImage=`url("${data2}")`;
-          
 
-    }
-   
-       
-   });
-    socket.on("sendBookedTableByServer",(data,data2)=>{
-        
-        let tbl=[];
-        let img=[];
-        for(let x in data){let inx=table.indexOf(data[x]);tbl.push(data[x]);table.splice(inx,1);}
-        for(let x in data2){let inx=PrfImageList.indexOf(data2[x]);img.push(data2[x]);PrfImageList.splice(inx,1);}
-        if(Object.keys(data).length>0)
-        {
-            for(let i=0;i<tbl.length;i++)
-            {
-        let ptbl=document.querySelector(`.${tbl[i]}`);
-        ptbl.style.backgroundImage=`url("${img[i]}")`;
+    });
+
+    socket.on("sendMessge", (data) => {
+        let msg = `${data} has joined the table`;
+        Message(msg);
+    });
+    socket.on("sendBookedImageAndTable", (data, data2, data3) => {
+        console.log(data);
+        if (data == "table5") {
+            let ptbl = document.querySelector(`.${MyChangesTable}`);
+            ptbl.style.backgroundImage = `url("${data2}")`;
+
+
+        }
+        else {
+            let ptbl = document.querySelector(`.${data}`);
+            ptbl.style.backgroundImage = `url("${data2}")`;
+
+
+        }
+
+
+    });
+    socket.on("sendBookedTableByServer", (data, data2) => {
+
+        let tbl = [];
+        let img = [];
+        for (let x in data) { let inx = table.indexOf(data[x]); tbl.push(data[x]); table.splice(inx, 1); }
+        for (let x in data2) { let inx = PrfImageList.indexOf(data2[x]); img.push(data2[x]); PrfImageList.splice(inx, 1); }
+        if (Object.keys(data).length > 0) {
+            for (let i = 0; i < tbl.length; i++) {
+                let ptbl = document.querySelector(`.${tbl[i]}`);
+                ptbl.style.backgroundImage = `url("${img[i]}")`;
             }
         }
-        if(table.length==0)
-    {
-        alert("Table is Full");
-        document.querySelector(".btnMainDiv").style.display="none";
-        return;
-    }
+        if (table.length == 0) {
+            alert("Table is Full");
+            document.querySelector(".btnMainDiv").style.display = "none";
+            return;
+        }
 
-        let Rtable=RandomTable();
-        MyChangesTable=Rtable;
-    let RImage=RandomProfileImage();
-    let Mytable=document.querySelector(`.${Rtable}`);
-    let table5=document.querySelector(".table5");
-    let card1=document.querySelector(`#Mycard1`);
-    let card2=document.querySelector(`#Mycard1`);
-    let Rcard=returnCardId(returnTableClass(Rtable));
-    MyChangesCard1=Rcard.card1;
-    MyChangesCard2=Rcard.card2;
-    card1.classList.remove("#Mycard1");
-    card2.classList.remove("#Mycard2");
-    card1.classList.add(Rcard.card1);
-    card2.classList.add(Rcard.card2);
-    let Rcard1=document.querySelector(`#${Rcard.card1}`);
-    let Rcard2=document.querySelector(`#${Rcard.card2}`);
-    Rcard1.classList.remove(Rcard.card1);
-    Rcard2.classList.remove(Rcard.card2);
-    Rcard1.classList.add("Mycard1");
-    Rcard2.classList.add("Mycard2");
-    table5.classList.remove("table5");
-    table5.classList.add(Rtable);
-    Mytable.classList.remove(Rtable);
-    Mytable.style.backgroundImage=`url("${RImage}")`;
-    Mytable.classList.add("myProfile");
-    socket.emit("ChangesTable",Rtable);
-    socket.emit("SendBookedTableList",{bookedBy:`${socket.id}`,bookedTable:`${Rtable}`,bookedProfileImage:`${RImage}`});
+        let Rtable = RandomTable();
+        MyChangesTable = Rtable;
+        let RImage = RandomProfileImage();
+        let Mytable = document.querySelector(`.${Rtable}`);
+        let table5 = document.querySelector(".table5");
+        let card1 = document.querySelector(`#Mycard1`);
+        let card2 = document.querySelector(`#Mycard1`);
+        let Rcard = returnCardId(returnTableClass(Rtable));
+        MyChangesCard1 = Rcard.card1;
+        MyChangesCard2 = Rcard.card2;
+        card1.classList.remove("#Mycard1");
+        card2.classList.remove("#Mycard2");
+        card1.classList.add(Rcard.card1);
+        card2.classList.add(Rcard.card2);
+        let Rcard1 = document.querySelector(`#${Rcard.card1}`);
+        let Rcard2 = document.querySelector(`#${Rcard.card2}`);
+        Rcard1.classList.remove(Rcard.card1);
+        Rcard2.classList.remove(Rcard.card2);
+        Rcard1.classList.add("Mycard1");
+        Rcard2.classList.add("Mycard2");
+        table5.classList.remove("table5");
+        table5.classList.add(Rtable);
+        Mytable.classList.remove(Rtable);
+        Mytable.style.backgroundImage = `url("${RImage}")`;
+        Mytable.classList.add("myProfile");
+        socket.emit("ChangesTable", Rtable);
+        socket.emit("SendBookedTableList", { bookedBy: `${socket.id}`, bookedTable: `${Rtable}`, bookedProfileImage: `${RImage}` });
 
-         });
-   // socket.on("sendBookedImageByServer",(data)=>{for(let x in data){bookedImage.push(data[x])}});
-    
+        socket.emit("LetsStartGame");
+    });
+    // socket.on("sendBookedImageByServer",(data)=>{for(let x in data){bookedImage.push(data[x])}});
+
+
+
+ 
+
+
+
+
 
 });
-socket.on("userLeft",(data)=>
-{
+socket.on("userLeft", (data) => {
 
-    let msg=`${data} Left the table.`;
+    let msg = `${data} Left the table.`;
     Message(msg);
 });
 
-socket.on("UpdateTableAndImage",(data,data2)=>{
-    let tbl=data;
-    let img=data2;
+
+
+
+
+socket.on("UpdateTableAndImage", (data, data2) => {
+    let tbl = data;
+    let img = data2;
     table.push(tbl);
     PrfImageList.push(img);
-    document.querySelector(`.${tbl}`).style.backgroundImage=`url("${addProfileImage}")`;
+    document.querySelector(`.${tbl}`).style.backgroundImage = `url("${addProfileImage}")`;
 });
+socket.on("BetCoinByUsers",(data)=>{
+    let msg=`${data.Name} calls  ${rupeesIcon} ${data.Coin}`;
+    Message(msg);
+
+
+});
+socket.on("BetCoinBySelf",(data)=>{
+    let msg=`You calls  ${rupeesIcon}${data.Coin}`;
+    Message(msg);
+
+
+});
+
+
+
 
--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 
-const express=require("express");
+const express = require("express");
 
 // const req=require("../public");
-const path=require("path");
-const app=express();
-const server=require("http").createServer(app);
-const io=require("socket.io")(server);
-const PORT=3000;
+const path = require("path");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
 // console.log(path.join(__dirname ,"../public"));
-app.use(express.static(path.join(__dirname ,"../public")));
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname ,"../public"));
+app.use(express.static(path.join(__dirname, "../public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public"));
 });
 // const getVisitor=()=>{
 //     let client=io.sockets.client().connected;
@@ -25,170 +25,232 @@ app.get("/",(req,res)=>{
 
 //Function For card Distribution
 
-  ///Creating 52 Cards....................
-  let deck=[];
-  //     Hearts,Clubs,Diamonds,Spades
-  let symbol=["H","C","D","S"];
-  let Ranks=["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
-  for(let i=0;i<symbol.length;i++)
-  {
-      for(let j=0;j<Ranks.length;j++)
-      {
-          deck.push(`${Ranks[j]}${symbol[i]}`);
-      }
-  }
+///Creating 52 Cards....................
+let deck = [];
+//     Hearts,Clubs,Diamonds,Spades
+let symbol = ["H", "C", "D", "S"];
+let Ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+for (let i = 0; i < symbol.length; i++) {
+    for (let j = 0; j < Ranks.length; j++) {
+        deck.push(`${Ranks[j]}${symbol[i]}`);
+    }
+}
 //................Deck is Created..................
 
 //Function For return Random Card and Remove that Card...........
-function ReturnRandomCard()
-{
-    let RandomIndex=Math.floor(Math.random()*deck.length);
-    let RandomCard=deck[RandomIndex];
-    deck.splice(RandomIndex,1);
+function ReturnRandomCard() {
+    let RandomIndex = Math.floor(Math.random() * deck.length);
+    let RandomCard = deck[RandomIndex];
+    deck.splice(RandomIndex, 1);
     return RandomCard;
 }
 
 
 
-let user={};
-let bookedTable={};
-let bookedImage={};
-let userCard=[];
+let user = {};
+let bookedTable = {};
+let bookedImage = {};
+let userCard = [];
+let BetCoinByuser={};
+
 //Function For set Card For User...........
-function setCard(user)
-{
-    for(let i in user)
-    {
-        let userId=i;
-        let card1=ReturnRandomCard();
-        let card2=ReturnRandomCard();
-        let table=bookedTable[userId];
-        let obj={};
-        obj["userId"]=userId,obj["card1"]=card1,obj["card2"]=card2,obj["table"]=table;
+function setCard(user) {
+    for (let i in user) {
+        let userId = i;
+        let card1 = ReturnRandomCard();
+        let card2 = ReturnRandomCard();
+        let table = bookedTable[userId];
+        let obj = {};
+        obj["userId"] = userId, obj["card1"] = card1, obj["card2"] = card2, obj["table"] = table;
         userCard.push(obj);
-        
+
 
     }
 
 }
 //Function For PlayGame.......
 
-   //Game started or Not.....................
-let start=false;
+//Game started or Not.....................
+let start = false;
 
 
 
-io.on("connection",(socket)=>
-{
-   
-   
-    socket.on("newUser",(Name)=>{
-       
+io.on("connection", (socket) => {
+
+
+    socket.on("newUser", (Name) => {
+
         console.log(`${Name} Joined`);
-        socket.emit("userJoin",Name);
-        if(start==true)
-        {
-            let msg="please Wait....";
-            socket.emit("gameStartedOrNot",msg);
+        socket.emit("userJoin", Name);
+        if (start == true) {
+            let msg = "please Wait....";
+            socket.emit("gameStartedOrNot", msg);
         }
-        socket.broadcast.emit("sendMessge",Name);
-        socket.emit("sendBookedTableByServer",bookedTable,bookedImage);
-        
-        user[socket.id]=Name;
-        socket.emit("sendUser",user);
-         if(Object.keys(user).length>0)
-         {
-             let time=5;
-             let send;
-            
-             if(start==false)
-             {
-                 send=setInterval(sendTimeToAll,1000);
-                start=true;
+        socket.broadcast.emit("sendMessge", Name);
+        socket.emit("sendBookedTableByServer", bookedTable, bookedImage);
 
-             }
-         
-             async function sendTimeToAll()
-          {
-              socket.emit("sendTime",time);
-              socket.broadcast.emit("sendTime",time);
-              
-              if(time==0)
-              {
-                 
-                socket.emit("sendGameStartMessage",time);
-                socket.broadcast.emit("sendGameStartMessage");
-                  clearInterval(send);
-                  setCard(user);
-                  socket.emit("cardDistribution",userCard);
-                  socket.broadcast.emit("cardDistribution",userCard);
-                 
-                  let Round=0;
-                  while(Round!=5)
-                  {
-                    
-                  for(let i=0;i<userCard.length;i++)
-                  {
-                      console.log("amit");
-                      socket.emit("PlayGame",userCard);
-                      socket.broadcast.emit("PlayGame",userCard);
-                      socket.on("RecevedCoin",(data,data2)=>{
-                             console.log(data,data2);
-                        })
-                       
-                      Round++;
-                      console.log(Round);
+        user[socket.id] = Name;
+        socket.emit("sendUser", user);
 
-                  }
+
+        //  if(Object.keys(user).length>0)
+        //  {
+        //      let time=5;
+        //      let send;
+
+        //      if(start==false)
+        //      {
+        //          send=setInterval(sendTimeToAll,1000);
+        //         start=true;
+
+        //      }
+
+        //      async function sendTimeToAll()
+        //   {
+        //       socket.emit("sendTime",time);
+        //       socket.broadcast.emit("sendTime",time);
+
+        //       if(time==0)
+        //       {
+
+        //         socket.emit("sendGameStartMessage",time);
+        //         socket.broadcast.emit("sendGameStartMessage");
+        //           clearInterval(send);
+        //           setCard(user);
+        //           socket.emit("cardDistribution",userCard);
+        //           socket.broadcast.emit("cardDistribution",userCard);
+
+        //           let Round=0;
+        //           while(Round!=5)
+        //           {
+
+        //           for(let i=0;i<userCard.length;i++)
+        //           {
+        //               console.log("amit");
+        //               socket.emit("PlayGame",userCard);
+        //               socket.broadcast.emit("PlayGame",userCard);
+        //               socket.on("RecevedCoin",(data,data2)=>{
+        //                      console.log(data,data2);
+        //                 })
+
+        //               Round++;
+        //               console.log(Round);
+
+        //           }
+        //         }
+
+
+        //       }
+        //       time--;
+
+        //   }
+
+
+
+
+        // }
+
+
+
+        let ChangesTable;
+        socket.on("ChangesTable", (data2) => {
+            ChangesTable = data2;
+        })
+        socket.on("SendBookedTableList", (data) => {
+
+            socket.broadcast.emit("sendBookedImageAndTable", data.bookedTable, data.bookedProfileImage, ChangesTable);
+
+
+            bookedTable[data.bookedBy] = data.bookedTable;
+            bookedImage[data.bookedBy] = data.bookedProfileImage;
+
+
+
+
+        });
+        socket.on("LetsStartGame", () => {
+            if (Object.keys(user).length > 1) {
+
+                let time = 5;
+                let send;
+
+                if (start == false) {
+                    send = setInterval(sendTimeToAll, 1000);
+                    start = true;
+
+                }
+
+                function sendTimeToAll() {
+                    socket.emit("sendTime", time);
+                    socket.broadcast.emit("sendTime", time);
+                    if (time == 0) {
+
+                        socket.emit("sendGameStartMessage", time);
+                        socket.broadcast.emit("sendGameStartMessage");
+                        clearInterval(send);
+                        setCard(user);
+                        socket.emit("cardDistribution", userCard);
+                        socket.broadcast.emit("cardDistribution", userCard);
+
+                    }
+
+
+
+                    time--;
                 }
 
 
-              }
-              time--;
-              
-          }
+            }
+
+        });
 
 
-         
 
-          }
-
-    });
- 
+    })
     
-    let ChangesTable;
-    socket.on("ChangesTable",(data2)=>{
-            ChangesTable=data2;
-        })
-    socket.on("SendBookedTableList",(data)=>{
+
+    ///////Logic For Receved Coin From Client......
+    socket.on("BetCoin",(data)=>{
         
-            socket.broadcast.emit("sendBookedImageAndTable",data.bookedTable,data.bookedProfileImage,ChangesTable);
-
-       
-       
-        bookedTable[data.bookedBy]=data.bookedTable;
-        bookedImage[data.bookedBy]=data.bookedProfileImage;
-    });
-
-
-
-
-
-    socket.on("disconnect",()=>{
-        console.log( `${socket.id} disconnect`);
+        if(BetCoinByuser[data.userId])
+        {
+        BetCoinByuser[data.userId]=Number(BetCoinByuser[data.userId])+Number(data.BetCoin);
+        }
+        else
+        {
+            BetCoinByuser[data.userId]=data.BetCoin;
+        }
+        let sendUserData={};
+        sendUserData["Name"]=user[data.userId];
+        sendUserData["Coin"]=BetCoinByuser[data.userId];
+        socket.broadcast.emit("BetCoinByUsers",sendUserData);
+        socket.emit("BetCoinBySelf",sendUserData);
         
-        socket.broadcast.emit("userLeft",user[socket.id]);
-        socket.broadcast.emit("UpdateTableAndImage",bookedTable[socket.id],bookedImage[socket.id]);
+      
+    })
+
+
+
+
+
+
+
+
+
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} disconnect`);
+
+        socket.broadcast.emit("userLeft", user[socket.id]);
+        socket.broadcast.emit("UpdateTableAndImage", bookedTable[socket.id], bookedImage[socket.id]);
         delete bookedTable[socket.id];
         delete bookedImage[socket.id];
         delete user[socket.id];
-        socket.emit("sendUser",userCard);
+        socket.emit("sendUser", userCard);
     });
 
 });
 
 
-server.listen(PORT,()=>
-{
+server.listen(PORT, () => {
     console.log(`Server is listning on port ${PORT}`);
 })
